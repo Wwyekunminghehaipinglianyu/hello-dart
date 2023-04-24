469e82ab5b700e05101ab88a3f5ac8dbaee359aa
启恩 王 <wangqien@mail.ynu.edu.cn>
2023-04-24 03:52:15 +0000

    4——24——

--- 31_async.dart
+++ 31_async.dart
@@ -2,6 +2,41 @@
 //AsyncCache<T> class
 //运行异步函数并将结果缓存一段时间。
 //此类的存在是为了涵盖具有潜在昂贵代码（例如文件 I/O、网络访问或隔离计算）的模式，这些代码不太可能快速更改并运行更少的时间
+
+
 import 'dart:async';
 
-void main() {}
+void runUsingFuture(List<String> args) {
+  // 调用 findEntryPoint() 函数，返回一个 Future 对象
+  findEntryPoint().then((entryPoint) {
+    // 在 Future 对象完成后，调用 runExecutable() 函数，返回一个新的 Future 对象
+    return runExecutable(entryPoint, args);
+  }).then((result) {
+    // 在新的 Future 对象完成后，调用 flushThenExit() 函数
+    flushThenExit(result);
+  });
+}
+
+Future<String> findEntryPoint() async {
+  // 模拟异步操作
+  await Future.delayed(Duration(seconds: 1));
+  // 返回一个字符串作为示例结果
+  return 'main';
+}
+
+Future<String> runExecutable(String entryPoint, List<String> args) async {
+  // 模拟异步操作
+  await Future.delayed(Duration(seconds: 2));
+  // 返回一个字符串作为示例结果
+  return 'Executable $entryPoint with args ${args.join(', ')}';
+}
+
+void flushThenExit(String result) {
+  // 输出结果到控制台
+  print(result);
+  // 退出程序
+}
+
+void main(List<String> args) {
+  runUsingFuture(args);
+}
--- 33_interation.dart
+++ 33_interation.dart
@@ -1,11 +1,40 @@
 //当我们需要创建一个类，使其可以在 for-in 循环中使用迭代器进行遍历时，
 //可以通过继承（如果可能的话）或实现 Iterable 接口来提供迭代器，并通过实现 Iterator 接口来定义实际的迭代操作。
-class Process{
-  //process
+import 'dart:math';
+
+class Process {
+  String name;
+  int pid;
+  
+  Process(this.name, this.pid);
+  
+  @override
+  String toString() => '$name (PID: $pid)';
 }
-class ProcessInterator implements Iterator<Process>{
+
+class ProcessIterator implements Iterator<Process> {
+  static const _processNames = ['Chrome', 'Safari', 'Firefox', 'Edge', 'Opera'];
+  static final _random = Random();
+  
+  int _count = 0;
+  
   @override
-  Process get current =>...
+  Process get current => Process(_processNames[_random.nextInt(_processNames.length)], _count);
+  
   @override
-  bool moveNext => ...
+  bool moveNext() {
+    _count++;
+    return _count <= 10;
+  }
+}
+
+class Processes extends IterableBase<Process> {
+  @override
+  final Iterator<Process> iterator = ProcessIterator();
+}
+
+void main() {
+  for (final process in Processes()) {
+    print(process);
+  }
 }
